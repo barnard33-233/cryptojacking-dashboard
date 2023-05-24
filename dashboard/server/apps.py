3f9e739e6dc7441af9b3e4042528416353e64498
barnard33-233 <htang0203@gmail.com>
2023-05-25 03:22:50 +0800

    feat: init database, finish framework of detector

--- dashboard/server/apps.py
+++ dashboard/server/apps.py
@@ -1,6 +1,67 @@
 from django.apps import AppConfig
-
+from django.apps import apps
+import datetime
+import threading
+from .tests import test_data
+# from .models import DeviceRecord, Modified
+mtime = ""
 
 class ServerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'server'
+
+    def ready(self) -> None:
+        global mtime
+        mtime = ""
+        DeviceRecord = apps.get_model('server', 'devicerecord')
+        Modified = apps.get_model('server', 'modified')
+        Modified_count = Modified.objects.count()
+        print(Modified_count)
+        if Modified_count == 0:
+            Modified.objects.create(mtime=datetime.datetime.now())
+        elif Modified_count > 1:
+            Modified.objects.all().delete()
+            Modified.objects.create(mtime=datetime.datetime.now())
+        else:
+            Modified.objects.all().update(mtime=datetime.datetime.now())
+        print(Modified.objects.count())
+        # start detector subprocess
+
+        start_detector()
+
+        print("Server started.")
+
+        return super().ready()
+
+
+# TODO: All things below
+class Detector:
+    def __init__(self) -> None:
+        pass
+
+    def packet_capture(self):
+        # call the real packet_capture here
+        return None
+        pass
+    
+    def classify(self, data):
+        # call the real classifier here
+        pass
+
+    def mainloop(self):
+        while True:
+            # get data
+            data = self.packet_capture()
+
+            # classify
+            result = self.classify(data)
+
+            # write it to database
+            # Test Here
+            # TODO
+
+
+def start_detector():
+    # new a Detector
+    # create a subprocess
+    pass
--- dashboard/server/tests.py
+++ dashboard/server/tests.py
@@ -1,3 +1,5 @@
 from django.test import TestCase
 
+test_data = []
+
 # Create your tests here.
